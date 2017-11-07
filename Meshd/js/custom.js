89945569143e61e275ae86043d5aa68074991725
Kishan Makani <makani20@gmail.com>
2017-11-07 12:42:08 +0530

    This is design templete

---
+++ Meshd/js/custom.js
@@ -0,0 +1,21 @@
+
+
+/** ==================================
+    Toggled Btn
+  ================================== **/
+
+  $("#btn-toggle").click(function(e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("btn-toggled");
+  });
+  $("#btn-toggle").click(function(e) {
+      e.preventDefault();
+      $(".admin-panel").toggleClass("right-top");
+  });
+
+
+  /** ==================================
+      Date-Picker
+    ================================== **/
+
+
